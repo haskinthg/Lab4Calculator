0d43a9e19b3e54583740c98ef102ba8774d5736f
haskinthg <chelnik.chaa@yandex.ru>
2021-10-25 16:38:51 +0300

    end

--- Calc_Wpf/MainWindow.xaml.cs
+++ Calc_Wpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Lab4CalcDll;
 
 namespace Calc_Wpf
 {
@@ -24,7 +25,42 @@ namespace Calc_Wpf
         {
             InitializeComponent();
         }
+        private void Clear_Box()
+        {
+            TextBox_1.Clear();
+            TextBox_2.Clear();
+            Result.Clear();
+        }
 
-       
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Result.Text = Calculator.addition(Double.Parse(TextBox_1.Text), Double.Parse(TextBox_2.Text)).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Вводите только числа!!!\nДробную часть отделяйте запятой");
+                Clear_Box();
+            }
+        }
+
+        private void Dif_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Result.Text = Calculator.difference(Double.Parse(TextBox_1.Text), Double.Parse(TextBox_2.Text)).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Вводите только числа!!!\nДробную часть отделяйте запятой");
+                Clear_Box();
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Clear_Box();
+        }
     }
 }
